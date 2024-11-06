9b35bc7661c730895953bc2d2dfafd3dae7ef38f
PaulinaB8 <paulinabruno343@gmail.com>
2024-11-06 09:04:29 -0300

    App routes completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { VistaPreviaComponent } from './pages/vista-previa/vista-previa.component';
+import { NotasComponent } from './pages/notas/notas.component';
+import { TodolistComponent } from './pages/todolist/todolist.component';
+import { CalendarioComponent } from './pages/calendario/calendario.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +18,17 @@ export const routes: Routes = [
     {
         path: "vista-previa", 
         component: VistaPreviaComponent,
+    },
+    {
+        path: "notas", 
+        component: NotasComponent,
+    },
+    {
+        path: "todolist", 
+        component: TodolistComponent,
+    },
+    {
+        path: "calendario", 
+        component: CalendarioComponent,
     },
 ];
