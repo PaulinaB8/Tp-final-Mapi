8c37e98c7670284d6a6f667495e93479f0ee039d
PaulinaB8 <paulinabruno343@gmail.com>
2024-11-05 23:31:11 -0300

    LocalStorage en Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { VistaPreviaComponent } from './pages/vista-previa/vista-previa.component';
 
 export const routes: Routes = [
     {
         path: "login", 
-        component: LoginComponent
+        component: LoginComponent,
+    },
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full",
+    },
+    {
+        path: "vista-previa", 
+        component: VistaPreviaComponent,
     },
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  router = inject(Router);
+
+datosLogin = {
+  usuario: "",
+  password : "",
+}
+
+  login(){
+    localStorage.setItem('usuario', "admin");
+    localStorage.setItem('contraseña', "admin");
+
+    console.log(this.datosLogin.usuario);
+
+    if(this.datosLogin.usuario === localStorage.getItem('usuario') && 
+    this.datosLogin.password === localStorage.getItem('contraseña')){
+      this.router.navigate(['/vista-previa']);
+    }else{
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Credenciales incorrectas. Por favor, vuelva a intentar",
+      });
+    }
+
+
+  }
 }
