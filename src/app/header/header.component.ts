c597a84bfb54fea224ec7a1b66e1a6aba42d1a61
cgorvalan <gorvalancandela@gmail.com>
2024-11-07 20:12:24 -0300

    Sidebar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -9,28 +10,7 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  router = inject(Router);
-
   
-
-  // openSidebar() {
-  //    this.getElementById("mySidebar").style.width = "250px";
-  // }
-  // Referencia al elemento con el id `mySidebar`
-  @ViewChild('mySidebar') mySidebar!: ElementRef;
-
-  constructor(private renderer: Renderer2) {}
-
-  // Función para abrir la barra lateral
-  openSidebar() {
-    this.renderer.setStyle(this.mySidebar.nativeElement, 'width', '250px');
-  }
-
-  // Función para cerrar la barra lateral
-  closeSidebar() {
-    this.renderer.setStyle(this.mySidebar.nativeElement, 'width', '0');
-  }
-
 
 
 }
