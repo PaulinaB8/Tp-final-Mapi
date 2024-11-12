ce98c963ca1a6c1e2e06d52b0b4dec38f931bdf0
cgorvalan <gorvalancandela@gmail.com>
2024-11-12 15:07:33 -0300

    12/11

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,50 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { routes } from '../app.routes';
-
-
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrls: ['./menu.component.scss']
 })
+export class MenuComponent {
+  @Input() isMenuComponentCollapsed = false;
 
-  export class MenuComponent {
-    isMenuComponentCollapsed= input.required<boolean>();
-  changeIsMenuComponentCollapsed = output<boolean>();
-  items = [
-    {
-      routeLink: 'vista-previa',
-      icon: 'fal fa-home',
-      label: 'VistaPrevia',
-    },
-    {
-      routeLink: 'todolist',
-      icon: 'fal fa-box-open',
-      label: 'Todolist',
-    },
-    {
-      routeLink: 'notas',
-      icon: 'fal fa-file',
-      label: 'Notas',
-    },
-    {
-      routeLink: 'calendario',
-      icon: 'fal fa-cog',
-      label: 'Calendario',
-    },
-  ];
+  constructor(private location: Location) {}
 
-  toggleCollapse(): void {
-    this.changeIsMenuComponentCollapsed.emit(!this.isMenuComponentCollapsed());
+  toggleSidebar() {
+    this.isMenuComponentCollapsed = !this.isMenuComponentCollapsed;
   }
 
-  closeSidenav(): void {
-    this.changeIsMenuComponentCollapsed.emit(true);
+  goBack() {
+    this.location.back(); // Navega a la página anterior
   }
-    
-  }
+}
+
