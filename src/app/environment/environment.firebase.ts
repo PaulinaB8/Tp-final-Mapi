a7648278b0d11255064548c3cb209a161370633d
PaulinaB8 <paulinabruno343@gmail.com>
2024-12-03 19:12:04 -0300

    Eliminar secretos

--- src/app/environment/environment.firebase.ts
+++ src/app/environment/environment.firebase.ts
@@ -1,9 +1,2 @@
-export const environment = {
-    apiKey: "AIzaSyD0G2TrnRLa8hzjWTOFEgGFkbFv1tahKvc",
-    authDomain: "mapi-73fcd.firebaseapp.com",
-    projectId: "mapi-73fcd",
-    storageBucket: "mapi-73fcd.firebasestorage.app",
-    messagingSenderId: "737907052656",
-    appId: "1:737907052656:web:7909e684e22538caae24b5"
-  };
+
   
