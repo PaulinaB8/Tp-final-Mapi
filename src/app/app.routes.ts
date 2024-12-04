3f8cf4e20dbc16dc63455e90c9e2fc9a0ceea0ce
PaulinaB8 <paulinabruno343@gmail.com>
2024-12-04 16:24:13 -0300

    Cambios routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,11 @@
-import { Router, Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { VistaPreviaComponent } from './pages/vista-previa/vista-previa.component';
 import { NotasComponent } from './pages/notas/notas.component';
 import { TodolistComponent } from './pages/todolist/todolist.component';
 import { CalendarioComponent } from './pages/calendario/calendario.component';
-import { AuthGoogleService } from './service/auth.google.service';
-import { inject } from '@angular/core';
 
 
-function guardaLogueado(){
-    let auth = inject(AuthGoogleService); 
-    let router = inject(Router);
-    if (auth.loginisTrue() !== null){
-        return true;
-    }else{
-        router.navigate(['/login']);
-        return false;
-    }
-}
 
 
 export const routes: Routes = [
@@ -33,7 +21,6 @@ export const routes: Routes = [
     {
         path: "vista-previa", 
         component: VistaPreviaComponent,
-        canActivate: [guardaLogueado]
     },
     {
         path: "notas", 
