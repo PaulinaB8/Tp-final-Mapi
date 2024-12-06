d88db32c986e7aa4edaf41bc47c4014abbbf4a11
PaulinaB8 <paulinabruno343@gmail.com>
2024-12-06 18:20:00 -0300

    Paso de funcionalidad al servicio Header

--- src/app/pages/vista-previa/vista-previa.component.ts
+++ src/app/pages/vista-previa/vista-previa.component.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from '../../header/header.component';
 import { TareasService } from '../../service/tareas.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { HeaderService } from '../../service/header';
 
 
 
@@ -19,6 +20,7 @@ export class VistaPreviaComponent implements OnInit{
 
   router = inject(Router);
   notas = inject(TareasService);
+  header = inject (HeaderService);
 
   notasExistentes : Nota[] = [];
   nota: Nota [] = [];
@@ -39,46 +41,12 @@ export class VistaPreviaComponent implements OnInit{
 
   getNotas(){
     this.notasExistentes = [];
-    this.notas.getNotas().then(r => {
-      let nota: Nota;
-      // console.log(r)
-      // console.log(r.length);
-  for (let item of r) { 
-    nota = {
-      id: item.id,
-      assigner_id: item.assigner_id,
-      assignee_id: item.assignee_id,
-      project_id: item.project_id,
-      section_id: item.section_id,
-      parent_id: item.parent_id,
-      order: item.order,
-      content: item.content,
-      description: item.description,
-      is_completed: item.is_completed,
-      labels: item.labels,
-      priority: item.priority,
-      comment_count: item.comment_count,
-      creator_id: item.creator_id,
-      created_at: item.created_at,
-      due: {
-        date: item?.due?.date, 
-        string: item?.due?.string, 
-        lang: item?.due?.lang,
-        is_recurring: item?.due?.is_recurring,
-      },
-      url: item.url,
-      duration: item.duration,
-      deadline: item.deadline,
-    }
-    // console.log(nota)
-    this.notasExistentes.push(nota) 
-  }
-  });
+    let r = this.header.getNotas();
+    this.notasExistentes = r;
   }
 
   verNota(id:number){
-    localStorage.setItem('id', id.toString())
-    this.router.navigate(['/nota-existente']);
+    this.header.verNota(id);
     }
   }
 
